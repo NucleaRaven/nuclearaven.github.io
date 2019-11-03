10858cdd1e4661fa2c0f39bde2ab29b699242c08
NucleaRaven <nuclearaven@gmail.com>
2019-11-03 22:46:13 +1100

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-object.oninput = function(){myScript};
+function validation(){
+    var box1=document.forms["help"]["1"].value;
+    var box2=document.forms["help"]["2"].value;
+if(box1!=""&&box2!=""){document.getElementById("subm").style.display="block";
+}else {document.getElementById("subm").style.display="none";
+
+}
+}
