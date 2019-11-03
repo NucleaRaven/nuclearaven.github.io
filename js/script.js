c7f8726bb0c49f58d65e85902608d10b747fa44d
NucleaRaven <nuclearaven@gmail.com>
2019-11-03 23:00:58 +1100

    y

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 function validation(){
-    var box1=document.forms["help"]["1"].value;
-    var box2=document.forms["help"]["2"].value;
-if(box1!=""&&box2!=""){document.getElementById("subm").style.display="block";
-}else {document.getElementById("subm").style.display="none";
-
+    var box1=document.forms["help"]["b1"].value;
+    var box2=document.forms["help"]["b2"].value;
+if(box1!=""&&box2!=""){
+    document.getElementById("submit").style.display="block";
+}else {
+    document.getElementById("submit").style.display="none";
 }
 }
